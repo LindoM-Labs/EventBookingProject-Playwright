0b7638ea2dbb55a24d0bcb96fbf73e1b8dd7bc0a
LindoM-Labs <makanda.lindokuhle@gmail.com>
2026-06-30 22:24:21 +0200

    Step 1 and 2 completed

---
+++ tests/EventBooking.spec.js
@@ -0,0 +1,52 @@
+import {test, expect} from '@playwright/test';
+
+//Login function
+async function login(page){
+    await page.goto('https://eventhub.rahulshettyacademy.com');
+    await page.getByPlaceholder('you@email.com').fill('makanda.odwa@gmail.com');
+    await page.getByLabel('Password').fill('Issie24!');
+    await page.locator('#login-btn').click();
+    await expect(page.getByRole('link' , {name: "Browse Events →"})).toBeVisible();
+}
+
+//Helper Function
+function futureDateValue(daysAhead = 7, hour, minute  = 0){
+    const date = new Date();
+    date.setDate(date.getDate() + daysAhead);
+    date.setHours(hour, minute, 0, 0);
+
+    const pad = (n) => String(n).padStart(2, '0');
+    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`;
+}
+
+test('Event Creation Test', async({browser}) =>
+{
+    //Step 1
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await login(page);
+
+    //Step 2
+   await page.getByRole('button' , {name: "Admin"}).click();
+   await expect(page.getByRole('link' , { name: "Manage Events"}).first()).toBeVisible();
+   await page.getByRole('link' , { name: "Manage Events"}).first().click();
+   await page.getByRole('heading' , { name: '+ New Event'}).waitFor();
+   const eventTitle = `Test Event ${Date.now()}`;
+   await page.locator('#event-title-input').fill(eventTitle);
+   await page.locator('#admin-event-form textarea').pressSequentially('Playwright workshop', {delay: 100});
+   const categoryOptions = page.locator('#category');
+   await categoryOptions.selectOption('Workshop')
+   await expect(page.getByLabel('Category')).toHaveValue('Workshop');
+   await page.getByLabel('City').fill('Cape Town');
+   await page.getByLabel('Venue').fill('CTICC Converntion Centre');
+   await page.getByLabel('Event Date & Time').fill(futureDateValue(10,14,45));
+   await page.getByLabel(/price/i).fill('100');
+   await page.getByLabel('Total Seats').fill('50');
+   await page.locator('#add-event-btn').click();
+   await expect(page.getByText('Event created!')).toBeVisible();
+   await page.pause();
+
+
+
+})
